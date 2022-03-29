6241df2417744bd1833fe980d0985bc9a43c134c
ayubch <ayoub.chahir96@gmail.com>
2022-03-29 15:48:57 +0000

    Use Redux in React Components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import Navbar from './components/Navigation';
 import Categories from './components/Categories';
 import BookContainer from './components/BookContainer';
+import store from './Redux/ConfigureStore';
 
 function App() {
   return (
-    <div className="app">
-      <Navbar />
-      <Switch>
-        <Route exact path="/">
-          <BookContainer />
-        </Route>
-        <Route path="/categories">
-          <Categories />
-        </Route>
-      </Switch>
-    </div>
+    <Provider store={store}>
+      <div className="app">
+        <Navbar />
+        <Switch>
+          <Route exact path="/">
+            <BookContainer />
+          </Route>
+          <Route path="/categories">
+            <Categories />
+          </Route>
+        </Switch>
+      </div>
+    </Provider>
   );
 }
 
