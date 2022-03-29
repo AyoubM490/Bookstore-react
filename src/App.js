da26e5c1a8da096dbb00a7fe635af907dadbc0f8
ayubch <ayoub.chahir96@gmail.com>
2022-03-29 15:47:14 +0000

    Add Files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import Navbar from './components/Navigation';
 import Categories from './components/Categories';
 import BookContainer from './components/BookContainer';
-import store from './Redux/ConfigureStore';
 
 function App() {
   return (
-    <Provider store={store}>
-      <div className="app">
-        <Navbar />
-        <Switch>
-          <Route exact path="/">
-            <BookContainer />
-          </Route>
-          <Route path="/categories">
-            <Categories />
-          </Route>
-        </Switch>
-      </div>
-    </Provider>
+    <div className="app">
+      <Navbar />
+      <Switch>
+        <Route exact path="/">
+          <BookContainer />
+        </Route>
+        <Route path="/categories">
+          <Categories />
+        </Route>
+      </Switch>
+    </div>
   );
 }
 
