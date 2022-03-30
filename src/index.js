a5c94cfcf8fa410af88db6ff45cd5c65a69e7fbb
ayubch <ayoub.chahir96@gmail.com>
2022-03-30 10:47:20 +0000

    Add Changes Requested by Code Review

--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import './index.css';
+import store from './Redux/ConfigureStore';
 import App from './App';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <Router>
       <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root'),
 );
