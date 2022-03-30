5f85e4cb34c7086c9cc5986c84da55ab80a5de30
ayubch <ayoub.chahir96@gmail.com>
2022-03-30 12:59:55 +0000

    Add Files

---
+++ src/FetchAPI/getId.js
@@ -0,0 +1,13 @@
+const baseUrl =
+  'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi';
+// const appId = 'DXx22TZCYfuKQX6UV8m5';
+
+const getId = async () => {
+  const response = await fetch(`${baseUrl}/apps/`, {
+    method: 'POST',
+  });
+  const data = response.text();
+  return data;
+};
+
+export default getId;
---
+++ src/FetchAPI/getPost.js
@@ -0,0 +1,19 @@
+/* eslint-disable camelcase */
+import { displayBook } from '../Redux/Books/Books';
+
+const getPOST = () => async (dispatch) => {
+  await fetch(
+    'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/DXx22TZCYfuKQX6UV8m5/books'
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      const bookObj = Object.entries(data);
+      const bookArray = bookObj.map(([item_id, value]) => {
+        const [eachBook] = value;
+        return { ...eachBook, item_id };
+      });
+      dispatch(displayBook(bookArray));
+    });
+};
+
+export default getPOST;
